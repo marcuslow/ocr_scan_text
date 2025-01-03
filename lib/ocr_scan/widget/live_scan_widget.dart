8e2a3edf91dafdd6b8f55554f73238190e56cfc4
Marcus <marcuslowwernchien@gmail.com>
2025-01-03 16:50:10 +0800

    solved!

--- lib/ocr_scan/widget/live_scan_widget.dart
+++ lib/ocr_scan/widget/live_scan_widget.dart
@@ -1,11 +1,63 @@
 import 'dart:async';
 import 'dart:io';
-
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:ocr_scan_text/ocr_scan/widget/scan_widget.dart';
+
+
+extension Nv21Converter on CameraImage {
+  Uint8List getNv21Uint8List() {
+    var width = this.width;
+    var height = this.height;
+
+    var yPlane = planes[0];
+    var uPlane = planes[1];
+    var vPlane = planes[2];
+
+    var yBuffer = yPlane.bytes;
+    var uBuffer = uPlane.bytes;
+    var vBuffer = vPlane.bytes;
+
+    var numPixels = (width * height * 1.5).toInt();
+    var nv21 = List<int>.filled(numPixels, 0);
+
+    // Full size Y channel and quarter size U+V channels.
+    int idY = 0;
+    int idUV = width * height;
+    var uvWidth = width ~/ 2;
+    var uvHeight = height ~/ 2;
+    // Copy Y & UV channel.
+    // NV21 format is expected to have YYYYVU packaging.
+    // The U/V planes are guaranteed to have the same row stride and pixel stride.
+    // getRowStride analogue??
+    var uvRowStride = uPlane.bytesPerRow;
+    // getPixelStride analogue
+    var uvPixelStride = uPlane.bytesPerPixel ?? 0;
+    var yRowStride = yPlane.bytesPerRow;
+    var yPixelStride = yPlane.bytesPerPixel ?? 0;
+
+    for (int y = 0; y < height; ++y) {
+      var uvOffset = y * uvRowStride;
+      var yOffset = y * yRowStride;
+
+      for (int x = 0; x < width; ++x) {
+        nv21[idY++] = yBuffer[yOffset + x * yPixelStride];
+
+        if (y < uvHeight && x < uvWidth) {
+          var bufferIndex = uvOffset + (x * uvPixelStride);
+          //V channel
+          nv21[idUV++] = vBuffer[bufferIndex];
+          //V channel
+          nv21[idUV++] = uBuffer[bufferIndex];
+        }
+      }
+    }
+    return Uint8List.fromList(nv21);
+  }
+}
+
 
 /// Widget allowing "live" scanning using the camera
 class LiveScanWidget extends ScanWidget {
@@ -92,55 +144,22 @@ class LiveScanWidgetState extends ScanWidgetState<LiveScanWidget> {
 
   /// Start image analysis
   Future _processCameraImage(CameraImage image) async {
-    final WriteBuffer allBytes = WriteBuffer();
-    for (final Plane plane in image.planes) {
-      allBytes.putUint8List(plane.bytes);
+    if (_controller == null) return;
+
+    final inputImage = cameraImageToInputImage(
+      image,
+      _cameras[0],
+      _controller!.value.deviceOrientation,
+    );
+
+    if (inputImage == null) {
+      debugPrint('Failed to convert CameraImage to InputImage.');
+      return;
     }
-
-    final bytes = allBytes.done().buffer.asUint8List();
-
-    final Size imageSize;
-
-    imageSize = Size(image.width.toDouble(), image.height.toDouble());
-
-    final camera = _cameras[0];
-    final imageRotation =
-        InputImageRotationValue.fromRawValue(camera.sensorOrientation);
-    if (imageRotation == null) return;
-
-    final inputImageFormat =
-        InputImageFormatValue.fromRawValue(image.format.raw as int);
-    if (inputImageFormat == null) return;
-
-    final planeData = image.planes.map(
-      (Plane plane) {
-        return InputImageMetadata(
-          bytesPerRow: plane.bytesPerRow,
-          size: Size(
-            plane.width?.toDouble() ?? 0,
-            plane.height?.toDouble() ?? 0,
-          ),
-          rotation: imageRotation,
-          format: inputImageFormat,
-        );
-      },
-    ).toList();
-
-    final inputImageData = InputImageMetadata(
-      size: imageSize,
-      format: inputImageFormat,
-      bytesPerRow: planeData.first.bytesPerRow,
-      rotation: InputImageRotation.rotation90deg,
-    );
-
-    final inputImage = InputImage.fromBytes(
-      bytes: bytes,
-      metadata: inputImageData,
-    );
 
     processImage(
       inputImage,
-      imageSize,
+      Size(image.width.toDouble(), image.height.toDouble()),
       null,
     );
   }
@@ -185,4 +204,76 @@ class LiveScanWidgetState extends ScanWidgetState<LiveScanWidget> {
     await _controller?.dispose();
     _controller = null;
   }
+}
+
+const _orientations = {
+  DeviceOrientation.portraitUp: 0,
+  DeviceOrientation.landscapeLeft: 90,
+  DeviceOrientation.portraitDown: 180,
+  DeviceOrientation.landscapeRight: 270,
+};
+
+InputImage? cameraImageToInputImage(
+  CameraImage image,
+  CameraDescription camera,
+  DeviceOrientation deviceOrientation,
+) {
+  final format = InputImageFormatValue.fromRawValue(image.format.raw);
+  if (format == null) {
+    return null;
+  }
+  final plane = image.planes.firstOrNull;
+  if (plane == null) {
+    return null;
+  }
+
+  final sensorOrientation = camera.sensorOrientation;
+
+  final InputImageRotation? rotation;
+  if (Platform.isIOS) {
+    rotation = InputImageRotationValue.fromRawValue(sensorOrientation);
+  } else if (Platform.isAndroid) {
+    var rotationCompensation = _orientations[deviceOrientation];
+    if (rotationCompensation == null) {
+      return null;
+    }
+    if (camera.lensDirection == CameraLensDirection.front) {
+      // front-facing
+      rotationCompensation = (sensorOrientation + rotationCompensation) % 360;
+    } else {
+      // back-facing
+      rotationCompensation =
+          (sensorOrientation - rotationCompensation + 360) % 360;
+    }
+    rotation = InputImageRotationValue.fromRawValue(rotationCompensation);
+  } else {
+    rotation = null;
+  }
+
+  if (rotation == null) {
+    return null;
+  }
+
+  final Uint8List bytes;
+  if (Platform.isAndroid) {
+    bytes = image.getNv21Uint8List();
+  } else {
+    final allBytes = WriteBuffer();
+    for (final plane in image.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    bytes = allBytes.done().buffer.asUint8List();
+  }
+
+  return InputImage.fromBytes(
+    bytes: bytes,
+    metadata: InputImageMetadata(
+      size: Size(image.width.toDouble(), image.height.toDouble()),
+      rotation: rotation, // used only in Android
+      format: Platform.isAndroid
+          ? InputImageFormat.nv21
+          : format, // Hardcode format for Android
+      bytesPerRow: plane.bytesPerRow, // used only in iOS
+    ),
+  );
 }
